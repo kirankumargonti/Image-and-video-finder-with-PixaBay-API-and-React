b6ec805512efdd0cb5e3202658d57580e7917ef0
Kirankumar Gonti <kirankumar225rk@gmail.com>
2020-02-04 15:20:19 +0530

    Updates

---
+++ precache-manifest.edd809247ff0ec093df0c8c0f5ec8bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090313d68117179c8ecd8a27b66ed4cc",
+    "url": "/Image-and-video-finder-with-PixaBay-API-and-React/index.html"
+  },
+  {
+    "revision": "d1288e7167a7d30b3b9b",
+    "url": "/Image-and-video-finder-with-PixaBay-API-and-React/static/css/main.796e9f3e.chunk.css"
+  },
+  {
+    "revision": "ae30e4e4eba844e9ea8c",
+    "url": "/Image-and-video-finder-with-PixaBay-API-and-React/static/js/2.1e07ff71.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Image-and-video-finder-with-PixaBay-API-and-React/static/js/2.1e07ff71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1288e7167a7d30b3b9b",
+    "url": "/Image-and-video-finder-with-PixaBay-API-and-React/static/js/main.5a658ae4.chunk.js"
+  },
+  {
+    "revision": "7e46536ebbd20b2bc74b",
+    "url": "/Image-and-video-finder-with-PixaBay-API-and-React/static/js/runtime-main.a5d2026f.js"
+  }
+]);
